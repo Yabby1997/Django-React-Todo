38c03a0ab5050aa394dfb54ed9b7028091c3cc85
yabby <yabby@Seunghun-MacBookPro.local>
2021-01-03 15:13:53 +0900

    모델 클래스 이름 변경 user -> User

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import user
+from .models import User
 
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'completed')    #Admin 페이지에서 뭐부터 보여줄지
-
-admin.site.register(Todo, TodoAdmin)
+    list_display = ('id', 'name', 'email')   
+    
+admin.site.register(User, UserAdmin)
--- backend/user/models.py
+++ backend/user/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class user(models.Model):
+class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     created_at = models.DateTimeField(auto_now_add=True)
