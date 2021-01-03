b4adccfca61b55f18b7bf1ac378df97f33f5758c
yabby <yabby@Seunghun-MacBookPro.local>
2021-01-03 15:12:00 +0900

    migrate user

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import user
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'completed')    #Admin 페이지에서 뭐부터 보여줄지
+
+admin.site.register(Todo, TodoAdmin)
